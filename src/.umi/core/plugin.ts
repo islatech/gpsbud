83f255516bbc9c7f7d19beecce0c245bef69a92c
xcesiv <10799441+xcesiv@users.noreply.github.com>
2020-11-16 17:45:07 -0500

    chore: update correct-layout

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/xcesiv/work/code/src/github.com/islatech/gpsbud/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','locale','locale','request',],
-});
-
-export { plugin };
