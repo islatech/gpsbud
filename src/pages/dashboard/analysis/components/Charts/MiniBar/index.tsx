ab8c9d57a4cc4e6851944f72a7a8970f6f169f1c
Maced <68704172+meitsaidesign@users.noreply.github.com>
2020-11-15 23:01:53 -0400

    Modifying code for GPSBud

--- src/pages/dashboard/analysis/components/Charts/MiniBar/index.tsx
+++ src/pages/dashboard/analysis/components/Charts/MiniBar/index.tsx
@@ -16,7 +16,7 @@ export interface MiniBarProps {
 }
 
 const MiniBar: React.FC<MiniBarProps> = (props) => {
-  const { height = 0, forceFit = true, color = '#1890FF', data = [] } = props;
+  const { height = 0, forceFit = true, color = '#FFE3A2', data = [] } = props;
 
   const scale = {
     x: {
