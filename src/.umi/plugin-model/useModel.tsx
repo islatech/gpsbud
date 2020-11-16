0fb45cfb8cda9a58b586d79b0bf1000062590aac
Maced <68704172+meitsaidesign@users.noreply.github.com>
2020-11-15 22:56:49 -0400

    Accessing ant design code

--- src/.umi/plugin-model/useModel.tsx
+++ src/.umi/plugin-model/useModel.tsx
@@ -1,9 +1,9 @@
 // @ts-nocheck
 import { useState, useEffect, useContext, useRef } from 'react';
 // @ts-ignore
-import isEqual from '/Users/xcesiv/work/code/src/github.com/islatech/gpsbud/node_modules/@umijs/plugin-model/node_modules/fast-deep-equal/index.js';
+import isEqual from '/Users/meiacevedo/GitHub/gpsbud/node_modules/@umijs/plugin-model/node_modules/fast-deep-equal/index.js';
 // @ts-ignore
-import { UmiContext } from '/Users/xcesiv/work/code/src/github.com/islatech/gpsbud/node_modules/@umijs/plugin-model/lib/helpers/constant';
+import { UmiContext } from '/Users/meiacevedo/GitHub/gpsbud/node_modules/@umijs/plugin-model/lib/helpers/constant';
 import { Model, models } from './Provider';
 
 export type Models<T extends keyof typeof models> = Model<T>[T]
