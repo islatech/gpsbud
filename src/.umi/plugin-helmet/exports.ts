ed245a237548539197971a9ec57d15ca1e6efce7
xcesiv <10799441+xcesiv@users.noreply.github.com>
2020-11-23 23:29:33 -0500

    fix: remove .umi & pro-layouts from src, add .umi to .gitignore

--- src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/meiacevedo/backup-correct-layout/node_modules/react-helmet';
--- src/pro-layout/es/WrapContent.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { CSSProperties } from 'react';
-declare const WrapContent: React.FC<{
-    isChildrenLayout?: boolean;
-    className?: string;
-    style?: CSSProperties;
-    location?: any;
-    contentHeight?: number | string;
-}>;
-export default WrapContent;
