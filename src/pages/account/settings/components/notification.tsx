0395ddf80ce0075198344af88f1f8277f8277e8f
xcesiv <10799441+xcesiv@users.noreply.github.com>
2020-11-26 02:37:35 -0500

    fix: styling:

--- src/pages/account/settings/components/notification.tsx
+++ src/pages/account/settings/components/notification.tsx
@@ -1,7 +1,7 @@
 import { List, Switch } from 'antd';
 import React, { Component, Fragment } from 'react';
 
-import { FormattedMessage, /* formatMessage */ } from 'umi';
+import { FormattedMessage /* formatMessage */ } from 'umi';
 
 type Unpacked<T> = T extends (infer U)[] ? U : T;
 
@@ -9,25 +9,56 @@ class NotificationView extends Component {
   getData = () => {
     const Action = (
       <Switch
-        checkedChildren={<FormattedMessage id="accountandsettings.settings.open" defaultMessage="Qu Lili" />}
-        unCheckedChildren={<FormattedMessage id="accountandsettings.settings.close" defaultMessage="Qu Lili" />}
+        checkedChildren={
+          <FormattedMessage id="accountandsettings.settings.open" defaultMessage="Qu Lili" />
+        }
+        unCheckedChildren={
+          <FormattedMessage id="accountandsettings.settings.close" defaultMessage="Qu Lili" />
+        }
         defaultChecked
       />
     );
     return [
       {
-        title: [<FormattedMessage id="accountandsettings.notification.password" defaultMessage="Qu Lili" />],
-        description: [<FormattedMessage id="accountandsettings.notification.password-description" defaultMessage="Qu Lili" />],
+        title: [
+          <FormattedMessage
+            id="accountandsettings.notification.password"
+            defaultMessage="Qu Lili"
+          />,
+        ],
+        description: [
+          <FormattedMessage
+            id="accountandsettings.notification.password-description"
+            defaultMessage="Qu Lili"
+          />,
+        ],
         actions: [Action],
       },
       {
-        title: [<FormattedMessage id="accountandsettings.notification.messages" defaultMessage="Qu Lili" />],
-        description: [<FormattedMessage id="accountandsettings.notification.messages-description" defaultMessage="Qu Lili" />],
+        title: [
+          <FormattedMessage
+            id="accountandsettings.notification.messages"
+            defaultMessage="Qu Lili"
+          />,
+        ],
+        description: [
+          <FormattedMessage
+            id="accountandsettings.notification.messages-description"
+            defaultMessage="Qu Lili"
+          />,
+        ],
         actions: [Action],
       },
       {
-        title: [<FormattedMessage id="accountandsettings.notification.todo" defaultMessage="Qu Lili" />],
-        description: [<FormattedMessage id="accountandsettings.notification.todo-description" defaultMessage="Qu Lili" />],
+        title: [
+          <FormattedMessage id="accountandsettings.notification.todo" defaultMessage="Qu Lili" />,
+        ],
+        description: [
+          <FormattedMessage
+            id="accountandsettings.notification.todo-description"
+            defaultMessage="Qu Lili"
+          />,
+        ],
         actions: [Action],
       },
     ];
@@ -40,7 +71,7 @@ class NotificationView extends Component {
         <List<Unpacked<typeof data>>
           itemLayout="horizontal"
           dataSource={data}
-          renderItem={(item: { actions: any; title: any; description: any; }) => (
+          renderItem={(item: { actions: any; title: any; description: any }) => (
             <List.Item actions={item.actions}>
               <List.Item.Meta title={item.title} description={item.description} />
             </List.Item>
