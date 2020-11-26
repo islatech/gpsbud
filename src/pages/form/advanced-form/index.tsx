fc8babb742113891889a03edcd73c840dac911c1
Maced <68704172+meitsaidesign@users.noreply.github.com>
2020-11-26 17:24:50 -0400

    adding upload image to product page to finish

--- src/pages/form/advanced-form/index.tsx
+++ src/pages/form/advanced-form/index.tsx
@@ -1,9 +1,11 @@
-import { CloseCircleOutlined } from '@ant-design/icons';
-import { Button, Card, Col, Form, Input, Popover, Row } from 'antd';
+import { CloseCircleOutlined, InboxOutlined } from '@ant-design/icons';
+import { Button, Card, Col, Divider, Form, Input, Popover, Row } from 'antd';
 import React, { FC, useState } from 'react';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import { connect, Dispatch } from 'umi';
 import TableForm from './components/TableForm';
+import { Upload, message } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import styles from './style.less';
 
 type InternalNamePath = (string | number)[];
@@ -130,75 +132,24 @@ const AdvancedForm: FC<AdvancedFormProps> = ({ submitting, dispatch }) => {
   const onFinishFailed = (errorInfo: any) => {
     setError(errorInfo.errorFields);
   };
-  /*
-  function getBase64({ img, callback }: { img: Blob; callback: { (imageUrl: any): void; (arg0: string | ArrayBuffer | null): any; }; }) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-  }
-I */
-  /*
-    function beforeUpload(file: { type: string; size: number; }) {
-      const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-      if (!isJpgOrPng) {
-        message.error('You can only upload JPG/PNG file!');
+  const { Dragger } = Upload;
+
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
       }
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isLt2M) {
-        message.error('Image must smaller than 2MB!');
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
       }
-      return isJpgOrPng && isLt2M;
-    }
-    */
-  /*
-    class Avatar extends React.Component {
-      state = {
-        loading: false,
-      };
-
-      handleChange = info => {
-        if (info.file.status === 'uploading') {
-          this.setState({ loading: true });
-          return;
-        }
-        if (info.file.status === 'done') {
-          // Get this url from response in real world.
-          getBase64({
-              img: info.file.originFileObj, callback: imageUrl => this.setState({
-                imageUrl,
-                loading: false,
-              })
-            },
-          );
-        }
-      };
-
-
-      render() {
-        const { loading, imageUrl } = this.state;
-        const uploadButton = (
-          <div>
-            {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div style={{ marginTop: 8 }}>Upload</div>
-          </div>
-        );
-        return (
-
-          <Upload
-            name="avatar"
-            listType="picture-card"
-            className="avatar-uploader"
-            showUploadList={false}
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            beforeUpload={beforeUpload}
-            onChange={this.handleChange}
-          >
-            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-          </Upload>
-        );
-      }
-    }
-    */
+    },
+  };
   return (
     <Form
       form={form}
@@ -210,62 +161,20 @@ I */
     >
       <PageContainer content="Upload all of your store products and keep track of them.">
         <Card title="Product Image" className={styles.card} bordered={false}>
-          <Row gutter={16}>
-            {/* <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        icon={<PlusOutlined/>}
-      >
-        {/* {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton} */}
-
-            {/* <Upload
-        name="avatar"
-        listType="picture-card"
-        className="avatar-uploader"
-        showUploadList={false}
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        icon={<PlusOutlined/>}
-      >
-        {/* {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton} */}
-
-            {/* <Col lg={6} md={12} sm={24}>
-              <Form.Item
-                label={fieldLabels.name}
-                name="name"
-                rules={[{ required: true, message: 'Please enter Warehouse Name and surname' }]}
-              >
-                <Input placeholder="Please enter Warehouse Name and surname" />
-              </Form.Item>
-            </Col> */}
-            {/* <Col xl={{ span: 6, offset: 2 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-              <Form.Item
-                label={fieldLabels.url}
-                name="url"
-                rules={[{ required: true, message: 'Please select' }]}
-              >
-                <Input
-                  style={{ width: '100%' }}
-                  addonBefore="http://"
-                  addonAfter=".com"
-                  placeholder="Please enter"
-                />
-              </Form.Item>
-            </Col> */}
-            {/* <Col xl={{ span: 8, offset: 2 }} lg={{ span: 10 }} md={{ span: 24 }} sm={24}>
-              <Form.Item
-                label={fieldLabels.owner}
-                name="owner"
-                rules={[{ required: true, message: 'Please select administration' }]}
-              >
-                <Select placeholder="Please select administration">
-                  <Option value="xiao">Fu Xiaoxiao</Option>
-                  <Option value="mao">Chow Mau Mau</Option>
-                </Select>
-              </Form.Item>
-            </Col> */}
+        
+          <Row gutter={24}>
+            <Col lg={24} md={24} sm={24}>
+            <div>
+              <Dragger  {...props}>
+                  <p id="draggerItem" className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p className="ant-upload-text">Click this area to upload image</p>
+                  
+              </Dragger>
+            </div>
+            </Col>
+            
           </Row>
         </Card>
         <Card title="Basic Info" className={styles.card} bordered={false}>
@@ -314,24 +223,7 @@ I */
                 </Input>
               </Form.Item>
             </Col>
-            {/* <Col xl={{ span: 6, offset: 2 }} lg={{ span: 8 }} md={{ span: 12 }} sm={24}>
-              <Form.Item
-                label={fieldLabels.dateRange2}
-                name="dateRange2"
-                rules={[{ required: true, message: 'Please enter' }]}
-              >
-                <TimePicker
-                  placeholder="Reminder Time"
-                  style={{ width: '100%' }}
-                  getPopupContainer={(trigger) => {
-                    if (trigger && trigger.parentNode) {
-                      return trigger.parentNode as HTMLElement;
-                    }
-                    return trigger;
-                  }}
-                />
-              </Form.Item>
-            </Col> */}
+            
             <Col xl={{ span: 8, offset: 2 }} lg={{ span: 10 }} md={{ span: 24 }} sm={24}>
               <Form.Item
                 label={fieldLabels.type2}
