b27ec5ec9d25cc2c71c54d6ab56de44d4b1c6756
xcesiv <10799441+xcesiv@users.noreply.github.com>
2020-11-27 01:05:47 -0500

    fix: remove bulk files from gpsbud-code

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/meiacevedo/GitHub/gpsbud/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','locale','locale','request',],
-});
-
-export { plugin };
