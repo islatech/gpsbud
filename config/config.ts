462d2d5f7eca613d2a3d2276731ae5d41f809b5e
Maced <68704172+meitsaidesign@users.noreply.github.com>
2020-11-27 20:08:03 -0400

    vscode told me to fix this import

--- config/config.ts
+++ config/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'umi';
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
+
 const { REACT_APP_ENV } = process.env;
 export default defineConfig({
   hash: true,
